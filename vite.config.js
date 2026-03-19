390a4cfc28f8189ad584a07fed225dfb61841fb1
KonstantinBYC <124351062+KonstantinBYC@users.noreply.github.com>
2026-03-19 21:20:37 +0200

    19-03 4b

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,15 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 
 export default defineConfig({
-  base: './', 
+  base: './',  
   root: 'public',
+  publicDir: false,   
   build: {
     rollupOptions: {
       input: glob.sync('./public/*.html'),
     },
     outDir: '../dist',
+    emptyOutDir: true,
   },
   plugins: [injectHTML(), FullReload(['./public/**/**.html'])],
 });
